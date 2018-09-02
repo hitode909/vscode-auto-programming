6530073c8f6e1f90f7cf0f9011222cc7c6d905ce
hitode909 <hitode909@gmail.com>
2018-09-02 15:32:45 +0900

    WIP

---
+++ src/Collector.ts
@@ -0,0 +1,140 @@
+import { exec } from 'child_process';
+import { quote } from 'shell-quote';
+
+type ItemList = { text: string, count: number }[];
+
+export class Collector {
+  private projectRoot: string;
+
+constructor(projectRoot: string) {
+    this.projectRoot = projectRoot;
+  }
+
+  // " hi " to "hi"
+  trim (str: string): string {
+    return str.replace(/^\s+/, '').replace(/\s+$/, '');
+  }
+
+  // "hi there" to "there"
+  shorten (str: string): string {
+    return str.replace(/^\s*\S*\s*/, '');
+  }
+
+  // (my $a =, =, my $b = 2) to my $a = 2
+  merge (original: string, query: string, found: string): string {
+    const index1 = original.indexOf(query);
+    const index2 = found.indexOf(query);
+    return original.substr(0, index1) + found.substr(index2);
+  }
+
+  horizontal(query: string, skip: number): Promise<ItemList> {
+    query = this.trim(query);
+    return new Promise((resolve) => {
+      if (!query) {
+        return resolve([]);
+      }
+
+      if (!skip) {
+        skip = 0;
+      }
+
+      var shortQuery = query;
+      for (var i = 0; i < skip; i++) {
+        shortQuery = this.shorten(shortQuery);
+      }
+
+      if (!shortQuery) {
+        resolve([]);
+      }
+
+      const command = quote(['git', 'grep', '--fixed-strings', '-h', shortQuery]);
+
+      exec(command, {cwd: this.projectRoot}, (err, stdout, stderr) => {
+        const lines = stdout.split(/\n/);
+
+        // When not found, make query shorter and search again.
+        // For example, "const a = 1" -> "a = 1" -> "= 1" -> "1".
+        if (lines.length === 0) {
+          return this.horizontal(query, ++skip).then((candidates) => {
+            resolve(candidates);
+          });
+        }
+
+        const counts: { [key: string]: number}  = {};
+
+        lines.forEach(line => {
+          const candidate = this.merge(query, shortQuery, this.trim(line));
+          if (candidate.length === 0) {
+            return;
+          }
+          if (candidate === shortQuery) {
+            return;
+          }
+          if (!counts[candidate]) {
+            counts[candidate] = 0;
+          }
+          counts[candidate] += 1;
+        });
+
+        const result = this.summarize(counts);
+        resolve(result);
+      });
+    });
+  }
+
+  vertical (query: string): Promise<ItemList>  {
+    return new Promise((resolve) => {
+      if (!query) {
+        return resolve([]);
+      }
+
+      const command = quote(['git', 'grep', '-A1', '--fixed-strings', '-h', query]);
+
+      exec(command, {cwd: this.projectRoot}, (err, stdout, stderr) => {
+        const lines = stdout.split(/\n/);
+
+        if (lines.length === 0) {
+          resolve([]);
+          return;
+        }
+
+        const counts: { [key: string]: number}  = {};
+        while(lines.length > 0) {
+          if (this.trim(lines.shift() || '') !== '--') {
+            continue;
+          }
+
+          const header = this.trim(lines.shift() || '');
+          if (!header) {
+            continue;
+           }
+
+           if (header !== query) {
+             continue;
+           }
+
+           const candidate = this.trim(lines.shift() || '');
+
+           if (!counts[candidate]) {
+             counts[candidate] = 0;
+           }
+           counts[candidate] += 1;
+        }
+
+        const result = this.summarize(counts);
+        resolve(result);
+      });
+    });
+  }
+
+  summarize (counts: { [key: string]: number}): ItemList {
+    const sorted = Object.keys(counts).sort((a, b)=> counts[b] - counts[a]);
+    const result = sorted.slice(0, 50).map(line => {
+      return {
+        text: line,
+        count: counts[line],
+      };
+    });
+    return result;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,58 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Collector } from './Collector';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+        const rootPath = vscode.workspace.rootPath;
+        if (!rootPath) { return; }
+        const collector = new Collector(rootPath);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "auto-programming" is now active!');
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) { return; }
+        const selection = editor.selection;
+        let lineContent = editor.document.lineAt(selection.anchor.line);
+        let line = selection.anchor.line;
+        if (!lineContent.text) {
+            lineContent = editor.document.lineAt(selection.anchor.line - 1);
+            collector.vertical(lineContent.text).then(items => {
+                selectItems(items, lineContent, line, collector);
+            });
+            return;
+        }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        collector.horizontal(lineContent.text, 0).then(items => {
+            selectItems(items, lineContent, line, collector);
+        });
     });
 
     context.subscriptions.push(disposable);
+}
+
+function selectItems(items: { text: string; count: number; }[], lineContent: vscode.TextLine, line: number, collector: Collector) {
+    vscode.window.showQuickPick(items.map(i => i.text)).then(item => {
+        if (!item) {
+            vscode.window.showErrorMessage('No candidate found');
+            return;
+        }
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        editor.edit(editBuilder => {
+            const indentLevel = ((lineContent.text.match(/^\s*/) || '')[0]).length;
+            const range = new vscode.Range(new vscode.Position(line, indentLevel), new vscode.Position(line, lineContent.text.length));
+            editBuilder.replace(range, item + "\n");
+        });
+        const next = new vscode.Position(line + 1, 0);
+        const newSelection = new vscode.Selection(next, next);
+        editor.selection = newSelection;
+
+        collector.vertical(item).then(items => {
+            selectItems(items, lineContent, line + 1, collector);
+        });
+    });
 }
 
 // this method is called when your extension is deactivated
